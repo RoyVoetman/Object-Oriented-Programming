1f980b580e7e8796a7faeb3425cdd9f7b735f28f
Roy Voetman <royvoetman@outlook.com>
2020-03-05 18:17:08 +0100

    Added boilerplate for state pattern

---
+++ src/nl/hanze/roy/oop/week3/state/ATMState.java
@@ -0,0 +1,16 @@
+package nl.hanze.roy.oop.week3.state;
+
+public interface ATMState {
+
+    void insertCard(ATM atm);
+
+    void ejectCard(ATM atm);
+
+    void insertPin(ATM atm);
+
+    void requestAmount(ATM atm);
+
+}
+
+// implement the 5 concrete states
+
---
+++ src/nl/hanze/roy/oop/week3/state/IdleState.java
@@ -0,0 +1,23 @@
+package nl.hanze.roy.oop.week3.state;
+
+public class IdleState implements ATMState {
+    @Override
+    public void insertCard(ATM atm) {
+
+    }
+
+    @Override
+    public void ejectCard(ATM atm) {
+
+    }
+
+    @Override
+    public void insertPin(ATM atm) {
+
+    }
+
+    @Override
+    public void requestAmount(ATM atm) {
+
+    }
+}
